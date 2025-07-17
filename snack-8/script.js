b6602f3ca66bed5df2be9d764f1d4562703c1898
Manuel-Lauriola <manuelauriol@gmail.com>
2025-07-17 17:17:25 +0200

    [UPD] - snack 8

--- snack-8/script.js
+++ snack-8/script.js
@@ -21,3 +21,7 @@ const students = [
 
 // Recupera la classe dello studente 'Marco Lanci'
 // Risultato: '3C'
+const marco = students.find((element)=>{
+  return element.name == `Marco Lanci`
+})
+console.log(marco.class)
