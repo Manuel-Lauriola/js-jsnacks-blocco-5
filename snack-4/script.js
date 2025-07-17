fab577fc60d00157cc6ef6df40ba0678f26b0d5b
Manuel-Lauriola <manuelauriol@gmail.com>
2025-07-17 16:46:30 +0200

    [UPD] - snack 4

--- snack-4/script.js
+++ snack-4/script.js
@@ -7,3 +7,7 @@ const posts = [
 
 // Crea un nuovo array coi nomi di tutti gli autori
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
+const authors = posts.map ((element)=>{
+  return element.author
+})
+console.log(authors)
